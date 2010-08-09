bee78d8f99f2012821c0a126c8714f8ed5fae586
Hans Malherbe <hansm@comunity.co.za>
2010-08-09 19:04:50 +0200

    BlackBerry merker teken nie as dit grys of ontgrys nie

--- DerevBb/src/bb/ui/BbMerker.java
+++ DerevBb/src/bb/ui/BbMerker.java
@@ -2,13 +2,16 @@ package bb.ui;
 
 import platform.ui.Kontrole;
 import platform.ui.Merk;
+import net.rim.device.api.ui.XYRect;
 import net.rim.device.api.ui.component.CheckboxField;
 import net.rim.device.api.ui.component.NullField;
 
 public class BbMerker implements Merk {
 	private final CheckboxField merker;
 	private NullField plekhouer;
+	private final KonkreteSkerm skerm;
 	public BbMerker(KonkreteSkerm konkreteSkerm, String etiket) {
+		skerm = konkreteSkerm;
 		merker = new CheckboxField(etiket, false);
 		konkreteSkerm.add(merker);
 	}
@@ -24,11 +27,20 @@ public class BbMerker implements Merk {
 
 	public Kontrole aktiveer() {
 		merker.setEditable(true);
+		tekenWeer();
 		return this;
+	}
+
+	private void tekenWeer() {
+		if (merker.isVisible()) {
+			XYRect posisie = merker.getContentRect();
+			skerm.invalidate(posisie.x, posisie.y, posisie.width, posisie.height);
+		}
 	}
 
 	public Kontrole deaktiveer() {
 		merker.setEditable(false);
+		tekenWeer();
 		return this;
 	}
 
